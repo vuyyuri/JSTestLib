6802174b04363f66329baffc50f964f96d56af41
vuyyuri <viyyure@yahoo.com>
2014-04-24 20:45:12 -0400

    Array initialization

--- map.js
+++ map.js
@@ -1,7 +1,6 @@
-var map=function(arrayArg, funcArg){
-	var resultArray= new Array();
-	for(var i=0, len= arrayArg.length;i<len; i++){
-		resultArray[i]=funcArg(arrayArg[i]);
-	}
-	return resultArray;
+var map=function(array, func){
+	var result= [];
+	for(var i=0, len= array.length;i<len; i++)
+		result.push(func(array[i]));
+	return result;
 };
